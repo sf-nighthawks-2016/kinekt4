56fb5e9bbac01c4e18b9710d964aa388fa509e07
Chris Otto & maxwellworkman & Ted Day-Fratto <pair+chrisotto.maxwellworkman.tedsf@devbootcamp.com>
2016-06-26 13:26:16 -0700

    Add Kinekt4 Add Kinekt4.prototype, #maxRows, #maxCols, #board Add Kinekt4.prototype, #whoseTurn, #notWhoseTurn, #addDot Add #showDot Add controller init, on click column Add css .red, .black

---
+++ public/js/application.js
@@ -0,0 +1,57 @@
+// Model
+var Kinekt4 = function() {
+};
+
+Kinekt4.prototype = {
+  maxRows: 6,
+  maxCols: 7,
+  board: [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ],
+  whoseTurn: function () {
+    var redCount = $.map(this.board, function(n) {
+      return n;
+    }).filter(function(color){
+      return color == "red";
+    }).length;
+    var blackCount = $.map(this.board, function(n) {
+      return n;
+    }).filter(function(color){
+      return color == "black";
+    }).length;
+    return (redCount > blackCount) ? "black" : "red";
+  },
+
+  notWhoseTurn: function () {
+    return (this.whoseTurn() === "red") ? "black" : "red";
+  },
+
+  addDot: function(col) {
+    if (this.board[col].length < this.maxRows) {this.board[col].push(this.whoseTurn())}
+  }
+};
+
+// Controller
+$(function() {
+  console.log("hello");
+  game = new Kinekt4();
+  game.whoseTurn();
+  // showWhoseTurn();
+
+  $("table#board td").on("click", function () {
+    var col = $($(this).closest("tr").find("td")).index(this);
+    game.addDot(col);
+    showDot(game.board[col].length,col);
+  })
+});
+
+// View
+var showDot = function (row,col) {
+  $("table#board tr:eq(-" + row + ") td:eq(" + col + ")").addClass(game.notWhoseTurn());
+}
