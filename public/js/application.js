2e177cf20044396f756b9c4b9040559dc57dd939
Chris Otto <email": null>
2016-06-26 23:30:12 -0700

    Add Kinekt4.prototype#gameOver, #check4

--- public/js/application.js
+++ public/js/application.js
@@ -9,7 +9,7 @@ Kinekt4.prototype = {
   board: [[],[],[],[],[],[],[]],
 
   // TODO refactor whoseTurn and notWhoseTurn to variable with default and toggle function
-  whoseTurn: function () {
+  whoseTurn: function() {
     var redCount = $.map(this.board, function(n) {
       return n;
     }).filter(function(color){
@@ -23,7 +23,7 @@ Kinekt4.prototype = {
     return (redCount > blackCount) ? "black" : "red";
   },
 
-  notWhoseTurn: function () {
+  notWhoseTurn: function() {
     return (this.whoseTurn() === "red") ? "black" : "red";
   },
 
@@ -34,20 +34,61 @@ Kinekt4.prototype = {
     } else {
       return false;
     }
+  },
+
+  check4: function(dot,dir) {
+    var endDot = {
+      row: dot.row + 3 * dir.row,
+      col: dot.col + 3 * dir.col
+    };
+    if (0 <= endDot.row && endDot.row < this.maxRows
+      && 0 <= endDot.col && endDot.col < this.maxCols) {
+      var dotCount = 1
+      for (var i = 1; i < 4; i++) {
+        if (this.board[dot.col][dot.row] !== this.board[dot.col + (i * dir.col)][dot.row + (i * dir.row)]) { break }
+        dotCount++
+      }
+      if (dotCount == 4) {return true} else {return false}
+    } else {return false}
+  },
+
+  gameOver: function(dot) {
+    var dir = {row: -1, col: 0}
+    var gameOver =
+      this.board.every(col => col.length == this.maxRows)
+      || this.check4(dot,dir);
+    for (var rowDir = -1; rowDir <= 1 && gameOver == false; rowDir++) {
+      dir = {row: rowDir, col: 1};
+      for (var i = 0; i < 4 && gameOver == false; i++) {
+        var tempDot = {
+          row: dot.row - (i * dir.row),
+          col: dot.col - (i * dir.col)
+        };
+        if (tempDot.row < 0 || this.maxRows <= tempDot.row || tempDot.col < 0) { break }
+        else {
+          gameOver = this.check4(tempDot,dir)
+        }
+      }
+    }
+    return gameOver;
   }
 };
 
 // Controller
 $(function() {
   game = new Kinekt4();
-  game.whoseTurn();
-  $("#turn").addClass("red");
+  $(".whose_turn").addClass(game.whoseTurn());
 
-  $("table#board td").on("click", function () {
+  $("table#board tr td").on("click", function() {
     var col = $($(this).closest("tr").find("td")).index(this);
     if (game.addDot(col)) {
-    showDot(game.board[col].length,col);
-    showTurn();
+      var dot = {
+        row: game.board[col].length - 1,
+        col: col
+      };
+      showDot(dot);
+      showWhoseTurn();
+      if (game.gameOver(dot)) {showWhoWon()}
     }
   })
   $(".btn").on("click", function(){
@@ -56,10 +97,14 @@ $(function() {
 });
 
 // View
-var showDot = function (row,col) {
-  $("table#board tr:eq(-" + row + ") td:eq(" + col + ")").addClass(game.notWhoseTurn());
+var showDot = function(dot) {
+  $("table#board tr:eq(-" + (dot.row+1) + ") td:eq(" + dot.col + ")").addClass(game.notWhoseTurn());
 }
 
-var showTurn = function () {
-  $("#turn").toggleClass("black red");
+var showWhoseTurn = function() {
+  $(".whose_turn").toggleClass("black red");
+}
+
+var showWhoWon = function() {
+  $("table#board").after("<p><div class='whose_turn " + game.notWhoseTurn() + "'></div> Won!</p>");
 }
